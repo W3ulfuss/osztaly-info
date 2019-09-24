41358a27e284648b7e7cccfd6c59fbbe8a84b530
Wulfus <47551810+W3ulfuss@users.noreply.github.com>
2019-09-24 09:38:24 +0300

    Add files via upload

---
+++ matrix_feladat_1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int n,m,i,x[1000][1000],j;
+int main()
+{
+    cin >> n;
+    cin >> m;
+    for (i=1;i<=n;i++)
+    {
+    for (j=1;j<=m;j++)
+    {
+    cout << "matrix[ " << i << "][ " << j<<"]";
+    cin >> x[i][j];
+    }
+    }
+    for (i=1;i<=n;i++)
+    {
+    for (j=1;j<=m;j++)
+    {
+    if (j==i)
+    {
+    x[i][j]=0;
+    }
+    if (i>j)
+    {
+    x[i][j]=-x[i][j];
+    }
+    cout << x[i][j] << "  ";
+    }
+    cout << endl;
+    }
+
+
+}
