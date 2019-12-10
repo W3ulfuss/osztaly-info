a2d842e6faa0156c3eb91a7f6bccf2c2c605d511
Wulfus <47551810+W3ulfuss@users.noreply.github.com>
2019-12-10 07:11:07 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
-#include <string>
+#include <stdlib.h>
 using namespace std;
-int sz=0;
+int n,m,i,x[1000][1000],j;
 int main()
 {
-   string a,b;
-   cin >> b;
-   sz=b.length();
-   if (b.length()<3)
-   {
-   cout << "nedterminat";
-   }
-   a.append(b,sz-3c,3);
-   cout << a;
+    cin >> n;
+    cin >> m;
+    for (i=1;i<=n;i++)
+    for (j=1;j<=m;j++)
+        cin >> x[i][j];
+    for (i=1;i<=n;i++){
+    for (j=1;j<=m;j++){
+        if (j==i) x[i][j]=0;
+        if (i>j) x[i][j]=-x[i][j];
+        cout << x[i][j] << "  ";
+    }
+    cout << endl;
+    }
 }
